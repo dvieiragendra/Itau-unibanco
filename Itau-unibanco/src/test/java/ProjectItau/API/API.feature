6ddf67a3f4055d1388fd9f5dae699a0cf08fdef1
dovd <dovd@BRPC014520.gft.com>
2021-05-04 10:08:57 -0300

    Finalização do teste com massa de dados

--- Itau-unibanco/src/test/java/ProjectItau/API/API.feature
+++ Itau-unibanco/src/test/java/ProjectItau/API/API.feature
@@ -1,12 +1,31 @@
-#language: pt
-
+#language: pt 
 
 Funcionalidade: Testes de API
   
-  @Pet
+  @PetSimples
   Cenario: Realizar a venda de um dog
     Dado A criação de um usuario chamado "Maria""Assunção" com id "1"
     E A criação de um pet chamado "Brutus" do tipo "dog" com id "1"
-    Quando Realizar a venda do pet para o usuario
+    Quando Realizar a venda do pet "1" para o usuario "1"
     E Mudar o status da ordem de venda para "delivered"
     Então Consultar se a ordem gerada esta correta
+     
+  @PetComMassa
+  Cenario: Realizar venda com massa de dados
+  Esquema do Cenário: Cenario com massa
+    Dado A criação de um usuario chamado "<primeiroNome>""<sobrenome>" com id "<idUser>"
+    E A criação de um pet chamado "<dog>" do tipo "<tipo1>" com id "<idPetDog>"
+    E A criação de um pet do tipo cat chamado "<cat>" do tipo "<tipo2>" com id "<idPetCat>"
+    Quando Realizar a venda do pet "<idPetDog>" para o usuario "<idUser>"
+    Quando Realizar a venda do pet "<idPetCat>" para o usuario "<idUser>"
+    E Mudar o status da ordem de venda para "<status1>"
+    E Mudar o status da ordem de venda do cat para "<status2>"
+    Então Consultar se a ordem gerada esta correta
+    
+    Exemplos:
+    	| primeiroNome	| sobrenome	| cat | dog	| tipo1	| tipo2	| status1	| status2 | idUser	| idPetDog	| idPetCat |
+    	| Maria |Macedo |Doris |Marley	|cachorro	|gato	|delivered	|approved	|2 | 2 | 3 |
+    	| Luiz	|Alves |Gatuti	|Brutus	|cachorro	|gato |delivered	|approved	| 3 | 4 | 5 |
+    	| Gerson |Vieira |Mili |Dic |cachorro |gato | delivered | approved | 4 | 6 | 7 |
+    	| Jessica | Figueiredo | Mingau | Xuxa | cachorro | gato | delivered | approved | 5 | 8 | 9 |
+    	| Natalie | Cavalcanti | Chiquinho | Luna | cachorro | gato | delivered | approved | 5 | 10 | 11 |
--- Itau-unibanco/src/test/java/ProjectItau/API/StepAPI.java
+++ Itau-unibanco/src/test/java/ProjectItau/API/StepAPI.java
@@ -1,9 +1,9 @@
 package ProjectItau.API;
 
-import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.E;
-import cucumber.api.java.pt.Então;
-import cucumber.api.java.pt.Quando;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
@@ -46,7 +46,7 @@ public class StepAPI {
 	@E("A criação de um pet chamado {string} do tipo {string} com id {string}")
 	public void criar_pet(String nomePet, String tipoPet, String id) {
 		 try {
-				RestAssured.baseURI = "https://petstore.swagger.io/v2/user";
+				RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
 		      
 		      response = given().log().all()
 		    		  .contentType("application/json")
@@ -83,16 +83,16 @@ public class StepAPI {
 		     }
 	}
 
-	@Quando("Realizar a venda do pet para o usuario")
-	public void realizar_venda_do_pet() {
+	@Quando("Realizar a venda do pet {string} para o usuario {string}")
+	public void realizar_venda_do_pet(String user, String pet) {
 		try {
 			RestAssured.baseURI = "https://petstore.swagger.io/v2/store/order";
 	      
 	      response = given().log().all()
 	    		  .contentType("application/json")
 	    		  .body("{\n"
-	    		  		+ "    \"id\": "+this.idUser+",\n"
-	    		  		+ "    \"petId\": "+this.idPet+",\n"
+	    		  		+ "    \"id\": "+user+",\n"
+	    		  		+ "    \"petId\": "+pet+",\n"
 	    		  		+ "    \"quantity\": 0,\n"
 	    		  		+ "    \"shipDate\": \"2021-05-03T20:17:29.569Z\",\n"
 	    		  		+ "    \"status\": \"placed\",\n"
@@ -144,7 +144,7 @@ public class StepAPI {
 	      JsonPath js = new JsonPath(response);
 	      String id = js.getString("status");
 	      
-	      if (id.equals("delivered")) {
+	      if (id.equals("delivered") || id.equals("approved")) {
 			System.out.println("Mensagem correta!!");
 		}else {
 			throw new Exception("status não correto");
@@ -154,5 +154,71 @@ public class StepAPI {
 	    	 RestAssured.reset();
 	     }
 	}
+	
+	@E("A criação de um pet do tipo cat chamado {string} do tipo {string} com id {string}")
+	public void criar_pet_cat(String nomePet, String tipoPet, String id) {
+		 try {
+				RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
+		      
+		      response = given().log().all()
+		    		  .contentType("application/json")
+		    		  .body("{\n"
+		    		  		+ "    \"name\": \""+nomePet+"\",\n"
+		    		  		+ "    \"photoUrls\": [\n"
+		    		  		+ "        \"<string>\",\n"
+		    		  		+ "        \"<string>\"\n"
+		    		  		+ "    ],\n"
+		    		  		+ "    \"id\": "+id+",\n"
+		    		  		+ "    \"category\": {\n"
+		    		  		+ "        \"id\": 0,\n"
+		    		  		+ "        \"name\": \""+tipoPet+"\"\n"
+		    		  		+ "    },\n"
+		    		  		+ "    \"tags\": [\n"
+		    		  		+ "        {\n"
+		    		  		+ "            \"id\": 0,\n"
+		    		  		+ "            \"name\": \"<string>\"\n"
+		    		  		+ "        },\n"
+		    		  		+ "        {\n"
+		    		  		+ "            \"id\": 0,\n"
+		    		  		+ "            \"name\": \"<string>\"\n"
+		    		  		+ "        }\n"
+		    		  		+ "    ],\n"
+		    		  		+ "    \"status\": \"available\"\n"
+		    		  		+ "}")
+		      .when().post()
+		      .then().assertThat().statusCode(200).extract().response().asString();
+		      this.idPet = id;
+		      System.out.println(response);
+		      
+		     }finally {
+		    	 RestAssured.reset();
+		     }
+	}
+	
+	@E("Mudar o status da ordem de venda do cat para {string}")
+	public void alterar_status_cat(String novoStatus) {
+		try {
+			RestAssured.baseURI = "https://petstore.swagger.io/v2/store/order";
+	      
+	      response = given().log().all()
+	    		  .contentType("application/json")
+	    		  .body("{\n"
+	    		  		+ "    \"id\": "+this.idUser+",\n"
+	    		  		+ "    \"petId\": "+this.idPet+",\n"
+	    		  		+ "    \"quantity\": 0,\n"
+	    		  		+ "    \"shipDate\": \"2021-05-03T20:17:29.569Z\",\n"
+	    		  		+ "    \"status\": \""+novoStatus+"\",\n"
+	    		  		+ "    \"complete\": true\n"
+	    		  		+ "}")
+	      .when().post()
+	      .then().assertThat().statusCode(200).extract().response().asString();
+	      System.out.println(response);
+	      
+	     }finally {
+	    	 RestAssured.reset();
+	     }
+	}
+	
+	
 	
 }
